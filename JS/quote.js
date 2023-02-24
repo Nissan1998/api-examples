26b755363e8f0c4614f4fa2c2ff000c817bbed73
Nissan1998 <jnnissanm@gmail.com>
2023-02-25 02:58:17 +0600

    kanye.restAPI

--- JS/quote.js
+++ JS/quote.js
@@ -1,1 +1,14 @@
-console.log('Hello World')
+const loadQuote = () => {
+    fetch('https://api.kanye.rest/')
+    .then(res => res.json())
+    .then(data => displayQuote(data))
+}
+
+const displayQuote = (quote) => {
+    console.log(quote)
+        const container = document.getElementById('quote');
+        container.innerHTML = `
+        <h1> ${quote.quote} </h1>
+        `
+    
+    }
