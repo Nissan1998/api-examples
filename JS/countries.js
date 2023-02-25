60e294fc5a6dc9850364e2a6e87cc2d7b2c9d107
Nissan1998 <jnnissanm@gmail.com>
2023-02-25 23:04:02 +0600

    allCountryWithDetails

--- JS/countries.js
+++ JS/countries.js
@@ -10,19 +10,40 @@ const displayCountries = (countries) =>{
 //     for(const country of countries){
 //         console.log(country)
 //     }
-countries.forEach(country => { console.log(country)
+countries.forEach(country => { console.log(country.cca2)
 const div = document.createElement('div');
-countriesContainer.classList.add('onCountry')
+div.classList.add('onCountry');
 div.innerHTML = `
-<h2 class="text-center text-2xl"> Name: <span class="font-bold"> ${country.name.common} </span> </h2>
-<p class="text-center"> ${country.capital? country.capital[0] : 'no capital'} </p>
+<h2 class="text-xl"> Country Name: ${country.name.common} </span> </h2>
+<p class="text-2xl"> Capital:- ${country.capital? country.capital[0] : 'no capital'} </p>
+<button onclick ="displayCountryDetails('${country.cca2}')" class=" px-5 py-1 rounded-xls mt-5 bg-orange-600"> Details </button> 
+
 `
 countriesContainer.appendChild(div)
 });
+}
+
+const displayCountryDetails = code =>{
+const url = `https://restcountries.com/v2/alpha/${code}`;
+fetch(url)
+.then(res => res.json())
+.then(data => showCountryDetail(data));
+}
+
+const showCountryDetail = country =>{
+    const countryDetailContainer = document.getElementById('country-detail')
+    countryDetailContainer.innerHTML =`
+    <h1 class ="tex-center font-bold"> Name:- ${country.name}</h1>
+    <h1 class ="tex-center font-bold"> Capital:- ${country.capital}</h1>
+    <h1 class ="tex-center font-bold"> Area:- ${country.area}</h1>
+    <h1 class ="tex-center font-bold"> Population:- ${country.population}</h1>
+    <img src="${country.flags.png}">
+
+    `
+
 }
 
     
 
 
 
-loadCountries()
