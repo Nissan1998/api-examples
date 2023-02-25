21495550afd43c7f2d31188d4a91bebd376b4c9a
Nissan1998 <jnnissanm@gmail.com>
2023-02-26 03:16:36 +0600

    searchMeals

--- JS/tailwind.config.js
+++ JS/tailwind.config.js
@@ -6,3 +6,22 @@ module.exports = {
     },
     plugins: [],
   }
+
+  module.exports = {
+    //...
+  
+    // add daisyUI plugin
+    plugins: [require("daisyui")],
+  
+    // daisyUI config (optional)
+    daisyui: {
+      styled: true,
+      themes: true,
+      base: true,
+      utils: true,
+      logs: true,
+      rtl: false,
+      prefix: "",
+      darkTheme: "dark",
+    },
+  }
